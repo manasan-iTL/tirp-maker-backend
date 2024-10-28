12959d0ee0940de953eeda93c421201c8a3f308e
manasan-iTL <daiyaofssh2020@gmail.com>
2024-10-28 21:10:53 +0900

    change: PlaceTypeとフロントの項目を一致させる

--- src/const/placeTypes.ts
+++ src/const/placeTypes.ts
@@ -2,8 +2,68 @@ export const PlaceType = {
     hotel: "HOTEL",
     eating: "EATING",
     sightseeing: "SIGHTSEEING",
-    amusement: "AMUSEMENT",
+    amusementPark: "AMUSEMENT_PARK",
+    themePark: "THEME_PARK",
+    hiking: "HIKING",
+    NaturalScenery: "NATURAL_SCENERY",
+    marineSports: "MARINE_SPORTS",
+    snowSports: "SNOW_SPORTS",
+    famousPlaces: "FAMOUS_PLACES",
+    MuseumArtGallery: "MUSEUM_ART_GALLERY",
+    craft: "CRAFT",
+    TraditionalCraft: "TRADITIONAL_CRAFT",
+    factory: "FACTORY",
+    zoo: "ZOO",
+    aquarium: "AQUARIUM", 
     depature: "DEPATURE",
     destination: "DESTINATION",
     must: "MUST",
 }
+
+export function convertJapanese(value: string): string {
+    switch (value) {
+        case PlaceType.themePark:
+            return "テーマパーク"
+    
+        case PlaceType.amusementPark:
+            return "遊園地"
+        
+        case PlaceType.hiking:
+            return "ハイキング"
+        
+        case PlaceType.NaturalScenery:
+            return "自然景観巡り"
+
+        case PlaceType.marineSports:
+            return "マリンスポーツ"
+
+        case PlaceType.snowSports:
+            return "スノースポーツ"
+        
+        case PlaceType.famousPlaces:
+            return "名所めぐり"
+        
+        case PlaceType.MuseumArtGallery:
+            return "博物館・美術館"
+        
+        case PlaceType.craft:
+            return "クラフト"
+        
+        case PlaceType.TraditionalCraft:
+            return "伝統工芸体験"
+        
+        case PlaceType.factory:
+            return "工場見学"
+
+        case PlaceType.zoo:
+            return "動物園"
+        
+        case PlaceType.aquarium:
+            return "水族館"
+            
+        default:
+            return "おすすめの観光スポット"
+    }
+}
+
+export type TPlaceType = typeof PlaceType

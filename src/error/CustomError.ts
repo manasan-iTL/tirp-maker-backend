527a871d0233491a9a42c49032c31d81fc2152d9
manasan-iTL <daiyaofssh2020@gmail.com>
2024-12-12 04:44:07 +0900

    カスタム例外の追加

---
+++ src/error/CustomError.ts
@@ -0,0 +1,31 @@
+export class ApiError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "ApiError"
+        Object.setPrototypeOf(this, ApiError.prototype); // 必須: TypeScriptでエラーオブジェクトを正しく継承
+    }
+}
+
+export class ValidationError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "ValidationError"
+        Object.setPrototypeOf(this, ValidationError.prototype); // 必須: TypeScriptでエラーオブジェクトを正しく継承
+    }
+}
+
+export class NotFoundThemeError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "NotFoundThemeError"
+        Object.setPrototypeOf(this, NotFoundThemeError.prototype); // 必須: TypeScriptでエラーオブジェクトを正しく継承
+    }
+}
+
+export class NotFoundRoutesError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "NotFoundRoutesError"
+        Object.setPrototypeOf(this, NotFoundRoutesError.prototype); // 必須: TypeScriptでエラーオブジェクトを正しく継承
+    }
+}
